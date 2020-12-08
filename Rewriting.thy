301039f12b0ed6f8ee1c410bf88fa6c6e464e800
Dawit <dawit.tirore@gmail.com>
2020-12-08 21:46:02 +0100

    add rewrite rules from section 3 and question about rule (d)

--- Rewriting.thy
+++ Rewriting.thy
@@ -1,5 +1,5 @@
   theory Rewriting
-    imports "MFODL_Monitor_Optimized.Formula" "MFODL_Monitor_Optimized.Regex"
+    imports "MFODL_Monitor_Optimized.Formula" "MFODL_Monitor_Optimized.Regex" "MFODL_Monitor_Optimized.Trace"
 begin
 
 (*
@@ -187,5 +187,19 @@ using sat_shift[of "[]"] by auto
 
 
 
+(*Section 3 of Normalization chapter p. 79*)
+lemma sat_3_a: "Formula.sat \<sigma> V v i (Formula.Neg (Formula.Neg \<alpha>)) = Formula.sat \<sigma> V v i \<alpha>" by auto
+lemma sat_3_b: "Formula.sat \<sigma> V v i (Formula.Exists (shiftI 0 \<alpha>)) = Formula.sat \<sigma> V v i \<alpha>" using sat_shift[of "[]"] by auto
+lemma sat_3_c1: "Formula.sat \<sigma> V v i (Formula.Neg(Formula.Or \<alpha> \<beta>)) = Formula.sat \<sigma> V v i (Formula.And (Formula.Neg \<alpha>) (Formula.Neg \<beta>)) " by auto
+lemma sat_3_c2: "Formula.sat \<sigma> V v i (Formula.Neg(Formula.And \<alpha> \<beta>)) = Formula.sat \<sigma> V v i (Formula.Or (Formula.Neg \<alpha>) (Formula.Neg \<beta>)) " by auto
+lemma sat_3_d: "Formula.sat \<sigma> V v i (Formula.Neg (Formula.Next I \<alpha>)) = Formula.sat \<sigma> V v i (Formula.Next I (Formula.Neg \<alpha>))" 
+  (*by (simp;auto;metis left_right linear of_nat_eq_enat of_nat_le_iff order_trans)*)
+  apply (auto simp add: not_le) (*MEETING: the first subgoal implying False, looks unsolvable, is that the case.
+                                           Applying auto followed by sledgehammer finds the proof
+                                           "metis left_right linear of_nat_eq_enat of_nat_le_iff order_tran" 
+                                           but when I use it the prover doesn't terminate. Could the rewrite be invalid?*)
+  sorry
 
+  
+  
 end
