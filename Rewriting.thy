b97212ad9b1da90a91077408f537e56e038001ec
Tirore96 <dawit.tirore@gmail.com>
2021-03-23 15:34:17 +0100

    Update Rewriting.thy

--- Rewriting.thy
+++ Rewriting.thy
@@ -1,4 +1,5 @@
-(*  This program is free software; you can redistribute it and/or modify
+(*  Copyright 2021 Dawit Legesse Tirore, Dmitriy Traytel, Martin Raszyk
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation; either version 2.1 of the License, or
     (at your option) any later version.
